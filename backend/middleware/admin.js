c4fec5d939f9249a09200eef2d53d1892790e8a1
Siddhesh Abnave <100532775+trigerfresh@users.noreply.github.com>
2026-02-12 01:32:03 +0530

    Delete backend directory

--- backend/middleware/admin.js
+++
@@ -1,8 +0,0 @@
-const admin = (req, res, next) => {
-  if (req.user.role !== 'admin') {
-    return res.status(403).json({ message: 'Admin access denied' })
-  }
-  next()
-}
-
-export default admin
